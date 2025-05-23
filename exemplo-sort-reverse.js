7b2a8772b1f21dbffa1f3262e3d09be474c76c1e
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 11:43:22 -0300

    mostra exemplos de sort e reverse

---
+++ exemplo-sort-reverse.js
@@ -0,0 +1,9 @@
+//sort - ordena os elementos do array
+let playlist = ['funk', 'hip-hop', 'pop', 'rock', 'jazz'];
+let playlistOrdenada = playlist.sort()
+
+console.log(`playlist ordenada: ${playlistOrdenada}`)
+
+//reverse - inverte a ordem dos elemntos do array
+let playlistInvertida = playlist.reverse()
+console.log(`playlist invertida: ${playlistInvertida}`)
