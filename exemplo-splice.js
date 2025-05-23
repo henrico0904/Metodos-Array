05a6ccbd5baab0992fbafef36080b53b5b66199c
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 10:37:01 -0300

    atualiza o exemplo de splice

--- exemplo-splice.js
+++ exemplo-splice.js
@@ -1,8 +1,10 @@
-let playlist = ['funk', 'hip-hop', 'pop', 'rock', 'jazz']
+let playlist = ['funk', 'hip-hop', 'pop', 'rock', 'jazz', 'hino']
 console.log(`antes do splice: ${playlist}`)
 
 //remover 2 elementos a partir do indice 2
 let elementosRemovidos = playlist.splice(2, 2)
+let desafio = playlist.splice(2, 3)
 
 console.log(`depois do splice ${playlist}`)
 console.log(`elementos removidos: ${elementosRemovidos}`)
+console.log(`elementos removidos: ${desafio}`)
