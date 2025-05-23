311cfd24f32b08135264ad4da6eee1c9109fa24d
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 11:26:45 -0300

    mostra exemplo do método de filter

---
+++ exemplo-filter.js
@@ -0,0 +1,4 @@
+let playlist = ['funk', 'hip-hop', 'pop', 'jazz']
+let playlistFiltrada = playlist.filter(musica => musica.length > 4)
+
+console.log(`a playlist filtrada (músicas mais de 4 letras): ${playlistFiltrada}`)
