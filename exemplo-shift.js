270ae58173b63faa3ee6391a83b858793e867385
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 08:52:30 -0300

    mostra exemplo de método shift

---
+++ exemplo-shift.js
@@ -0,0 +1,9 @@
+let playlist = ['funk', 'Hip-Hop', 'pop']
+
+console.log(`antes do shift: ${playlist}`)
+
+//removendo a primeira musica
+let musicaRemovida = playlist.shift();
+
+console.log(`depois do shift: ${playlist}`)
+console.log(`musica removida: ${musicaRemovida}`)
