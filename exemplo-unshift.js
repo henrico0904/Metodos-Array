83081fe8b6c153d221193deeb25501f7b5a2cbc3
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 08:57:50 -0300

    mostra exemplo de método unshift

---
+++ exemplo-unshift.js
@@ -0,0 +1,8 @@
+let playlist = ['funk', 'hip-hop', 'pop']
+
+console.log(`antes do unshift: ${playlist}`)
+
+//adcionando uma nova música no inicio da playlist
+playlist.unshift('rock')
+
+console.log(`depois do unshift: ${playlist}`)
