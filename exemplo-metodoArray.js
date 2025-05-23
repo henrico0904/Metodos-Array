fa984d5ee90a5b4292603a06dc6e50983a5b7c2b
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 08:38:25 -0300

    deleta parte do arquivo que já está em outro

--- exemplo-metodoArray.js
+++ exemplo-metodoArray.js
@@ -10,15 +10,5 @@ playlist.push('lofi')
 console.log(`depois do push: ${playlist}`)
 
 
-//exemplo de método de pop (tirar o ultimo objeto do array)
 
-let musicaRemovida = playlist.pop()
-
-console.log(`depois do pop ${playlist}`)
-console.log(`musica removida ${musicaRemovida}`)
-
-let segundaMusicaRemovida = playlist.pop()
-
-console.log(`depois do segundo pop ${playlist}`)
-console.log(`a musica removida foi ${segundaMusicaRemovida}`)
 
