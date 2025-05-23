f2573245479790c9670c49d4dcc643428ea3326b
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 11:01:57 -0300

    mostra exemplo de método map

---
+++ exemplo-map.js
@@ -0,0 +1,7 @@
+//modificar o array com letras maiusculas
+let playlist = ['funk', 'hip-hop', 'pop'];
+console.log(`playlist normal: ${playlist}`)
+
+let playlistEmCaps = playlist.map(musica => musica.toUpperCase());
+console.log(`playlist em maiúsculas: ${playlistEmCaps}`)
+
