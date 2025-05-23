eb29db87077036e00da995392bd0938e264d09da
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 10:02:53 -0300

    atualiza o exemplo de slice

--- exemplo-slice.js
+++ exemplo-slice.js
@@ -2,3 +2,4 @@ let playlist = ['funk', 'hip-hop', 'pop', 'rock', 'jazz', 'reggae', 'classico', 
 console.log(`parte da playlist (indice 2 a 5): ${playlist.slice(2, 6)}`)
 
 console.log(`parte da playlist (indice 0 a 3): ${playlist.slice(0, 4)}`)
+console.log(`apenas rock: ${playlist.slice(3, 4)}`)
