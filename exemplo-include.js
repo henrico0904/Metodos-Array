885550a1f1cab7a4f27361e02afcbadfcd6506c0
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-23 09:27:29 -0300

    mostra exemplo do método include

---
+++ exemplo-include.js
@@ -0,0 +1,4 @@
+let playlist = ['funk','hip-hop', 'pop', 'opera', 'hard-rock','ninar', 'jazz']
+
+console.log(`a playlist inclui Rock? ${playlist.includes('rock')}`)
+console.log(`a playlist inclui  Funk? ${playlist.includes('funk')}`)
